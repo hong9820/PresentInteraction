1f8ec983155b3b22626c6fd248535889f3cbec51
Jimmy Jeon <jhong9820@gmail.com>
2018-06-16 15:03:57 +0900

    처음의 ViewController에 EdgePanGesture 동작을 추가한다. EdgePanGesture 동작을 다룰 함수 추가

--- PresentInteratction/ViewController.swift
+++ PresentInteratction/ViewController.swift
@@ -19,6 +19,50 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  /**
+   EdgePanGestureRecognizer 동작을 설정한다.
+   
+   Parameters:
+   - view: EdgePanGesture 동작을 추가할 뷰
+   */
+  func setupBackGesture(view: UIView) {
+    let swipeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleBackGesture(_:)))
+    swipeGesture.edges = .left
+    view.addGestureRecognizer(swipeGesture)
+  }
+  
+  /**
+   Gesture에 대한 동작을 설정한다.
+   
+   Parameters:
+   - gesture: EdgePanGesture 동작의 결과값
+   */
+  @objc func handleBackGesture(_ gesture: UIScreenEdgePanGestureRecognizer) {
+    //gesture가 작동하는 view
+    let translation = gesture.translation(in: view)
+    //EdgePanGesture로 얼마나 이동했는지 계산함
+    let progress = translation.x / self.view.frame.width
+    print(progress)
+    
+    //gesture 상태에 따른 동작
+    switch gesture.state {
+    case .began:
+      break
+      
+    case .changed:
+      break
+      
+    case .cancelled:
+      break
+      
+    case .ended:
+      break
+      
+    default:
+      break
+    }
+  }
 
 
 }
