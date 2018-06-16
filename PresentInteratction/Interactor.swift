6c8aca210f3a1d855dc81bc02cd7387818cd09b3
Jimmy Jeon <jhong9820@gmail.com>
2018-06-16 15:48:40 +0900

    UIPercentDrivenInteractiveTransition를 상속받는 Interactor 클래스 생성

---
+++ PresentInteratction/Interactor.swift
@@ -0,0 +1,14 @@
+//
+//  Interactor.swift
+//  PresentInteratction
+//
+//  Created by Jin Hong Jeon on 2018. 6. 16..
+//  Copyright © 2018년 Jin Hong Jeon. All rights reserved.
+//
+
+import UIKit
+
+class Interactor: UIPercentDrivenInteractiveTransition {
+  var hasStarted = false
+  var shouldFinish = false
+}
